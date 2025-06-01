20c99d1b3e9ce8df1d83762e2459bf682e277aef
Manoela Garcia <151690205+manu-sgc@users.noreply.github.com>
2025-06-01 13:41:20 -0300

    Update 330.py

--- Codigos/330.py
+++ Codigos/330.py
@@ -5,16 +5,15 @@
         :type n: int
         :rtype: int
         """
-        miss = 1        # menor número que ainda não conseguimos formar
-        i = 0           # índice em nums
-        patches = 0     # número de valores adicionados
+        miss = 1       
+        i = 0           
+        patches = 0     
 
         while miss <= n:
             if i < len(nums) and nums[i] <= miss:
                 miss += nums[i]
                 i += 1
             else:
-                # patchar com 'miss' porque não há número em nums que o cubra
                 miss += miss
                 patches += 1
 
